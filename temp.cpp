2738393a14eaf3cac553048bfd00e853688c3811
singhanshuman01 <singhanshuman592@gmail.com>
2025-04-18 19:55:24 +0530

    wrote more server code

--- temp.cpp
+++ temp.cpp
@@ -1,18 +1,71 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
 
+static void msg(const char* msg){
+    fprintf(stderr, "%s\n", msg);
+}
 
-int fd = socket(AF_INET,SOCK_STREAM,0);
-int val = 1;
-int setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+static void die(const char* msg){
+    int err = errno;
+    fprintf(stderr, "[%d] %s\n", err, msg);
+    abort();
+}
 
-struct sockaddr_in addr = {};
+static void do_something(int connfd){
+    char rbuf[64] = {};
+    ssize_t n = read(connfd, rbuf, sizeof(rbuf)-1);
 
-addr.sin_family = AF_INET;
-addr.sin_port = htons(5000);
-addr.sin_addr.s_addr = htonl(0);
+    if(n<0){
+        msg("read() error");
+        return;
+    }
+    printf("client says: %s\n", rbuf);
 
-int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
+    char wbuf[] = "world";
+    write(connfd, wbuf, strlen(wbuf));
+}
 
-if(rv) { die("bind()"); }
+int main()
+{
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    int val = 1;
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+
+    struct sockaddr_in addr = {};
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(5000);
+    addr.sin_addr.s_addr = htonl(0);
+
+    int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
+
+    if (rv)
+    {
+        die("bind()");
+    }
+
+    rv = listen(fd, SOMAXCONN);
+    if (rv)
+    {
+        die("listen()");
+    }
+
+    while (true)
+    {
+        struct sockaddr_in client_addr = {};
+        socklen_t addrlen = sizeof(client_addr);
+        int connfd = accept(fd, (struct sockaddr *)&client_addr, addrlen);
+        if (connfd < 0)
+            continue;
+        do_something(connfd);
+        close(conn_fd);
+    }
+}
